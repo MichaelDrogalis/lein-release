4b4b992a1e0260cb9abbf6f2f49b9e09598afb14
Tom Coupland <tom.coupland@nokia.com>
2012-07-02 17:43:39 +0100

    lein-release plugin: preparing 1.0.35 release

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject lein-release/lein-release "1.0.35-SNAPSHOT"
+(defproject lein-release/lein-release "1.0.35"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in-leiningen true
   :lein-release {:deploy-via :clojars
                  :release-tasks [:clean :jar :pom :install]}
   :dependencies [[org.clojure/clojure "1.3.0"]])
-
