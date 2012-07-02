a22fde3c615371f329d94bf7901826de95402cff
Tom Coupland <tom.coupland@nokia.com>
2012-07-02 17:27:28 +0100

    lein-release plugin: preparing 1.0.32 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.31"
+(defproject lein-release/lein-release "1.0.32"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in-leiningen true
