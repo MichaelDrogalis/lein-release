71ec90c3415011101dc4fe15c5ae141e57a6050e
Tom Coupland <tom.coupland@nokia.com>
2012-07-02 16:48:18 +0100

    lein-release plugin: bumped version from 1.0.20 to 1.0.21-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.20"
+(defproject lein-release/lein-release "1.0.21-SNAPSHOT"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in-leiningen true
