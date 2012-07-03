cdc843d6be988eec750f65ef28b85a0ba9002ec9
Tom Coupland <tom.coupland@nokia.com>
2012-07-03 10:44:01 +0100

    lein-release plugin: preparing 1.0.46 release

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject lein-release/lein-release "1.0.46-SNAPSHOT"
+(defproject lein-release/lein-release "1.0.46"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in-leiningen true
   :lein-release {:deploy-via :clojars
                  :release-tasks [:clean :jar :pom :install]}
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :jvm-opts [
-             "-server" "-Xms128M" "-Xmx256M"
+  :jvm-opts ["-server" "-Xms128M" "-Xmx256M"
              ;; Use these options in development to allow debugging 
              ;; with jswat on localhost port 9900 
              "-Xdebug"
