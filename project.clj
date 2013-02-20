6f76357db415baa7643600588421504b1e2f9c58
Michael Drogalis <mjd3089@rit.edu>
2013-02-19 22:13:57 -0500

    lein-release plugin: preparing 1.0.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.1-SNAPSHOT"
+(defproject lein-release/lein-release "1.0.1"
   :description "Leiningen Release Plugin"
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]]
