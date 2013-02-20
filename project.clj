5801c62dc4084437380f868856cb33c276fe8699
Michael Drogalis <mjd3089@rit.edu>
2013-02-19 22:13:57 -0500

    lein-release plugin: bumped version from 1.0.1 to 1.0.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.1"
+(defproject lein-release/lein-release "1.0.2-SNAPSHOT"
   :description "Leiningen Release Plugin"
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]]
